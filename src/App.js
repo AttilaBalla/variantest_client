9d0eb03970b0a618c87d71d6a0df9bb2d1e46566
AttilaBalla <xattus@gmail.com>
2019-04-11 20:33:08 +0200

    project bootstrap / initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">Varian Test Project - Server Manager</a>
+        </nav>
+        <div className="container">
+          <ul className="list-group">
+            <li className="list-group-item">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Morbi leo risus</li>
+            <li className="list-group-item">Porta ac consectetur ac</li>
+            <li className="list-group-item">Vestibulum at eros</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
